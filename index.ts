0a4e2bcf5580227fb06dac12e8147b6a9f6a59b4
h316101 <weiwei.dai@honeywell.com>
2020-04-08 11:21:33 +0800

    2. fix连线的bug

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@
 export { UiModule } from './src/ui.module';
 export { WawaComponent } from './src/components/wawa/wawa.component';
 export { NavComponent } from './src/components/nav/nav.component';
+export {DragFlowComponent } from './src/components/drag-flow/drag-flow.component';
+export {WolkflowComponent } from './src/components/wolkflow/wolkflow.component';
