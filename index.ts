1d22af14e2326451cbbbe8bbb9d2bc8af5ce7c55
h316101 <weiwei.dai@honeywell.com>
2020-04-08 12:32:12 +0800

    1.delete import './styles.scss';

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import './src/styles.scss';
 export { UiModule } from './src/ui.module';
 export { WawaComponent } from './src/components/wawa/wawa.component';
 export { NavComponent } from './src/components/nav/nav.component';
