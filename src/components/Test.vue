ecf9f4a12a5c7df2091df812f17228cf86122cdc
h316101 <weiwei.dai@honeywell.com>
2020-09-28 09:30:03 +0800

    vue2

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <ol>
+        <li v-for="todo in todos" v-bind:key="todo.text">
+            {{ todo.text }}
+        </li>
+    </ol>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        props: {
+            todos:{
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
