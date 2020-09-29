93674154ce7d6d115ba724f2d4fd47bff1b0a9d6
h316101 <weiwei.dai@honeywell.com>
2020-09-29 22:51:24 +0800

    vue3 tree

--- vue3-study/src/components/HelloWorld.vue
+++ vue3-study/src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<!--  若传参  :load="fetchData" 则为懒加载，若不传此参数则为直接展开-->
+    <zf-tree ref="tree" :data="data" :draggable="true"
+             :load="fetchData" >
+      <!--template 为slot插槽展示的内容-->
+      <template v-slot="item">
+        {{item.key}}-{{item.name}}
+      </template>
+    </zf-tree>
+    <icon icon="icon-spinner2"  @click="getData()"></icon>
   </div>
 </template>
 
 <script>
+import ZfTree from "./tree/tree";
+import Icon from "./icon/icon";
 export default {
   name: 'HelloWorld',
+  components: {Icon, ZfTree},
   props: {
-    msg: String
-  }
+    msg: String,
+  },
+  methods:{
+    getData() {
+      console.log(this.$refs.tree.getCheckNodes());
+    },
+  },
+   data() {
+     return {
+       data: [{ key: 1, name: 'dww', children: [{
+               key: 11, name: 'dww11', children: [{
+                 key: 111, name: 'dww111', children: []
+               }, {
+                 key: 112, name: 'dww112', children: []
+               },{
+                 key: 113, name: 'dww113', children: []
+               }]
+             }
+           ]
+         }, {
+           key:2, name: 'dww2', children: []
+         }, {
+         key: 3, name: 'dww3', children: []
+       }],
+       fetchData: (data, cb) => {
+         // append假数据
+         const a = () => new Promise(resolve => {
+           setTimeout(() => {
+             resolve({key: 44, name: 'dww44', children: [
+                 {key: 441, name: 'dww441', children: []}
+               ]})
+           }, 3000);
+         });
+         const b = () => new Promise(resolve => {
+           setTimeout(() => {
+             resolve({key: 45, name: 'dww45', children: []})
+           }, 1000);
+         });
+          async function f() {
+            const res = [];
+            res.push(await a());
+            res.push(await b());
+            return  res;
+          }
+          f().then(res => {
+            cb(res);
+          });
+       }
+     }
+   },
+
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
